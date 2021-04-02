67292a60c7be173aeffa1c4c01a755f2ceb97163
Mohammad Kefah <mo@edraj.io>
2021-04-02 09:29:43 +0300

    fix reading bug

--- task1/protocol.go
+++ task1/protocol.go
@@ -23,35 +23,37 @@ func readMessage(conn io.Reader, wr io.Writer) error {
 	bs := make([]byte, 8)
 	conn.Read(bs)
 	size := binary.BigEndian.Uint64(bs)
-	buff := make([]byte, 4092)
+	buff := make([]byte, size)
 
-	var recv uint64 = 0
-	for {
-		if (size - recv) < 4092 {
-			buff := make([]byte, size-recv)
+	io.ReadFull(conn, buff)
+	wr.Write(buff)
+	// var recv uint64 = 0
+	// for {
+	// 	if (size - recv) < 4092 {
+	// 		buff := make([]byte, size-recv)
 
-			if _, err := conn.Read(buff); err != nil {
-				return err
-			}
+	// 		if _, err := conn.Read(buff); err != nil {
+	// 			return err
+	// 		}
 
-			if _, err := wr.Write(buff); err != nil {
-				return err
-			}
+	// 		if _, err := wr.Write(buff); err != nil {
+	// 			return err
+	// 		}
 
-			return nil
-		}
+	// 		return nil
+	// 	}
 
-		n, err := conn.Read(buff)
-		if err != nil {
-			return err
-		}
+	// 	n, err := conn.Read(buff)
+	// 	if err != nil {
+	// 		return err
+	// 	}
 
-		if _, err := wr.Write(buff); err != nil {
-			return err
-		}
+	// 	if _, err := wr.Write(buff); err != nil {
+	// 		return err
+	// 	}
 
-		recv += uint64(n)
-	}
+	// 	recv += uint64(n)
+	// }
 
 	return nil
 }
